4e038602ebcc88b2cf3f6069df39c09cd6a0c360
Kevin James Hilton <kjhilton01@gmail.com>
2019-12-03 21:30:16 -0800

    started membership page and added pics

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import OurMission from "./../layout/AboutUs.png";
+import OurMission from "./../layout/images/AboutUs.png";
 
 const Home = () => {
   return (
--- src/components/pages/Membership.js
+++ src/components/pages/Membership.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+//import Blanket from "./../layout/images/Blanket.jpg";
+//import Figurines from "./../layout/images/Figurines.jpg";
+//import Jars from "./../layout/images/Jars.jpg";
+import Pots from "./../layout/images/Pots.jpg";
+
+const negmargin = {
+  "margin-top": "-1rem"
+};
 
 const Membership = () => {
-  return <h1>Membership</h1>;
+  return (
+    <Fragment>
+      <div className="img-wrapper-banner" style={negmargin}>
+        <img src={Pots} alt="Pots"></img>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Membership;
